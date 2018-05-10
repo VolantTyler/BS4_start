4449d0bd473492d9413b72228adef82e7ee8c60a
Tyler Stahl <tyler@volantweb.com>
2018-05-10 16:33:36 -0400

    initial build

--- index.php
+++ index.php
@@ -4,60 +4,73 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="A portfolio of featured website projects from Tyler Stahl, web professional">
+    <meta name="author" content="Tyler Stahl">
     <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Volant Template for Bootstrap 4</title>
+    <title>Portfolio: Tyler Stahl</title>
 
+    <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400" rel="stylesheet">
+
+    <!-- CSS stylesheets -->
     <link href="css/bootstrap.css" rel="stylesheet">
-
     <link href="css/custom.css" rel="stylesheet">
   </head>
 
   <body>
-
-    <?php include("navigation.php"); ?>
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container">
-        <h1 class="display-3">Hello, world!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+    <header id="heading" class="container">
+      <div  class="row">
+        <div class="col-md-2">
+          <img src="images/volant_logo copy.png" class="img-fluid" alt="Volant Web Design logo">
+        </div>
+        <div class="col-md-10 heading-title">
+          <h1>Tyler Stahl</h1>
+          <p>Design Director, Volant Web Design</p>
+        </div>
       </div>
-    </div>
-
-    <div class="container">
-      <!-- Example row of columns -->
+    </header>
+    
+    <main class="container">
       <div class="row">
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        <div class="banner col-md-12">
+          <img src="images/take_flight.jpg" class="img-fluid" alt="canada geese in flight">
         </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <h3 id="sub-heading">Featured Work</h3>
         </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        <div class="featured col-md-4">
+          <img src="images/client-drdevlyn.png" class="img-fluid" alt="">
+          <h2 class="featured-title">Dr. Devlyn</h2>
+          <p class="featured-address">https://drdevlyn.com/</p>
+        </div>
+        <div class="featured col-md-4">
+          <img src="images/client-ccr.png" class="img-fluid" alt="">
+          <h2 class="featured-title">Christ Church Ridgewood</h2>
+          <p class="featured-address">http://christchurchridgewood.org/</p>
+        </div>
+        <div class="featured col-md-4">
+          <img src="images/client-arlene.png" class="img-fluid" alt="">
+          <h2 class="featured-title">Growing Up Strong</h2>
+          <p class="featured-address">https://growingupstrong.net/</p>
         </div>
       </div>
 
-      <hr>
-      <?php include("footer.php"); ?>
+    </main>
 
+    <footer class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <div class="text-muted">
+              &copy; <script>document.write(new Date().getFullYear())</script> <a href="https://volantweb.com/" target="_blank" style="color: #868e96">Volant Web Design</a> 
+          </div>
+        </div>
+      </div>
+    </footer>
 
-    </div> <!-- /container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- Bootstrap core JavaScript-->
     <script src="js/jquery.min.js"></script> 
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script> 
